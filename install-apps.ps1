6faf2aef99beee9f609939665adcb4146ff87ca4
Jan Falta <faltajan@gmail.com>
2024-08-07 09:53:48 +0200

    add app install

---
+++ install-apps.ps1
@@ -0,0 +1,8 @@
+# Install Google Chrome
+winget install -e --id Google.Chrome
+
+# Install Microsoft Windows Terminal
+winget install -e --id Microsoft.WindowsTerminal
+
+# Install Git
+winget install -e --id Git.Git
